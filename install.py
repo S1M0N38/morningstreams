f2d01f495e56022ac82cab1bb2fbc0bd7646a449
S1M0N38 <bertolottosimone@gmail.com>
2022-08-17 20:44:53 +0200

    Checking SHA265 during engine installation

--- install.py
+++ install.py
@@ -1,6 +1,7 @@
-import tarfile
+import hashlib
 import pathlib
 import shutil
+import tarfile
 import urllib.request
 
 here = pathlib.Path(__file__).parent
@@ -13,6 +14,7 @@ url = (
     "https://github.com/moromete/repository.moromete.addons/"
     "raw/master/plugin.video.streams/" + version
 )
+sha265 = "a4a73f84f33139ec5c0decdf1c1edc1a9d83737eac3ce6df2005e1099826e297"
 
 print("Removing old acestream engine...", end=" ", flush=True)
 path_tar_gz.unlink(missing_ok=True)
@@ -23,6 +25,14 @@ print("Downloading acestream.tar.gz...", end=" ", flush=True)
 with open(path_tar_gz, "wb") as f:
     with urllib.request.urlopen(url) as resp:
         f.write(resp.read())
+print("✓")
+
+print("Checking SHA256 for acestream.tar.gz...", end=" ", flush=True)
+hash = hashlib.sha256()
+with open(path_tar_gz, "rb") as f:
+    for block in iter(lambda: f.read(4096), b""):
+        hash.update(block)
+assert sha265 == hash.hexdigest()
 print("✓")
 
 print("Extracting to acestream.engine...", end=" ", flush=True)
